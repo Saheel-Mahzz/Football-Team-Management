c058a32271bd711931a5b068d6786030b6bff314
saheel <programmingwithsahil@gmail.com>
2025-12-10 13:33:52 +0545

    chore: refactor the classnmae for design

--- src/pages/team/index.tsx
+++ src/pages/team/index.tsx
@@ -2,7 +2,9 @@ import PlayerForm from "@/components/players/playerForm";
 
 export default function Team() {
   return (
-    <div className="flex flex-col w-1/2 mx-auto border border-gray-300 rounded-md p-4">
+<div className="flex flex-col w-full max-w-3xl mx-auto bg-white  rounded-xl shadow-2xl border border-gray-200 p-4">
+<h2 className="text-2xl font-semibold">Team Managment</h2>
+
 
       <PlayerForm/>
     </div>
