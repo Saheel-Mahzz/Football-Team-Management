1292ecd5f97633c96d8aa45d13e31fc8713b0dd2
saheel <programmingwithsahil@gmail.com>
2025-12-13 06:28:09 +0545

    feat: added a route for starting xi

---
+++ src/pages/startingXI/index.tsx
@@ -0,0 +1,10 @@
+
+export const XI_TEAM_ROUTE = '/starting-xi'
+
+export default function StartingXI() {
+  return (
+    <div>
+      I am starting xi team..
+    </div>
+  )
+}
--- src/router.tsx
+++ src/router.tsx
@@ -1,4 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
+import StartingXI, { XI_TEAM_ROUTE } from "./pages/startingXI";
 import Team from "./pages/team";
 
 
@@ -6,6 +7,10 @@ const router = createBrowserRouter([
     {
         path:'/',
         element:<Team/>
+    },
+    {
+        path:XI_TEAM_ROUTE,
+        element:<StartingXI/>
     }
 ])
 
