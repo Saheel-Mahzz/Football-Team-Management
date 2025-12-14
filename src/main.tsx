0a847478ef8623b689d0f7e86dc483283fdee525
saheel <programmingwithsahil@gmail.com>
2025-12-14 08:38:05 +0545

    refactor: deleted context and commented code

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { Toaster } from './components/ui/sonner.tsx'
-import { TeamProvider } from './context/TeamContext.tsx'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
@@ -22,8 +21,6 @@ createRoot(document.getElementById('root')!).render(
     }
   }}
 />
-    <TeamProvider>
     <App />
-    </TeamProvider>
   </StrictMode>,
 )
--- src/pages/team/utils/teamRules.ts
+++ src/pages/team/utils/teamRules.ts
@@ -3,21 +3,6 @@ import { Player } from "@/types/players";
 export const validateSquadLimit =(players:Player[])=> players.length >= 22
 
 export const validateJerseyNumber=(players:Player[],jerseyNumber:number,excludeId?:number) =>{
-    console.log('players',players,jerseyNumber)
     return players.some((p)=> p.jerseyNumber === jerseyNumber && p.id !== excludeId)
 }
 
-// export const validatePositionMinimums = (players: Player[], newPlayer: Player) => {
-//   const getCount = (pos: Position) => 
-//     players.filter(p => p.position === pos).length + (newPlayer.position === pos ? 1 : 0);
-
-//   const checks = [
-//     { pos: 'GK', min: 1, count: getCount('GK') },
-//     { pos: 'DEF', min: 4, count: getCount('DEF') },
-//     { pos: 'MID', min: 4, count: getCount('MID') },
-//     { pos: 'FWD', min: 2, count: getCount('FWD') }
-//   ];
-
-//   const failed = checks.find(c => c.count < c.min);
-//   return failed ? `Need at least ${failed.min} ${failed.pos} player${failed.min > 1 ? 's' : ''}` : null;
-// };
