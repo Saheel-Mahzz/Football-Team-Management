261e5bd5a7781a54374f0d24c894483bbd7e442e
saheel <programmingwithsahil@gmail.com>
2026-06-30 21:33:10 +0545

    fix: deleted some unwanted files

--- src/router.tsx
+++ src/router.tsx
@@ -1,5 +1,4 @@
 import { createBrowserRouter } from "react-router-dom";
-import DynamicForm, { DYNAMIC_FORM_ROUTE } from "./pages/dynamicTypeForm/dynamicForm";
 import StartingXI, { XI_TEAM_ROUTE } from "./pages/startingXI";
 import Tasks, { TASK_ROUTE } from "./pages/TaskList";
 import Team from "./pages/team";
@@ -14,10 +13,7 @@ const router = createBrowserRouter([
         path:XI_TEAM_ROUTE,
         element:<StartingXI/>
     },
-    {
-        path:DYNAMIC_FORM_ROUTE,
-        element:<DynamicForm/>
-    },
+ 
     {
         path:TASK_ROUTE,
         element:<Tasks/>
